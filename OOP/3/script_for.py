70898c8a6d2cf6fb12f805fc42b2260d7fc46fba
HoVitaliy <tyrwekpofa@gmail.com>
2023-11-07 18:15:13 +0200

    pract_3

---
+++ OOP/3/script_for.py
@@ -0,0 +1,15 @@
+#Цикл «for».
+
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+
+#Цикли "while".
+
+# Prints out 0,1,2,3,4
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # This is the same as coun
